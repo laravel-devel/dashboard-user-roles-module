29d0106b6e38ad91dcdf3cd6deaa511615f539d9
Alexander Zavyalov <whmordred@gmail.com>
2020-03-29 13:47:02 +0800

    Additional CRUD tests

--- Tests/Feature/CrudTest.php
+++ Tests/Feature/CrudTest.php
@@ -124,4 +124,60 @@ class CrudTest extends TestCase
 
         $this->assertDatabaseMissing('devel_user_roles', ['key' => $role->key]);
     }
+
+    /** @test */
+    public function the_default_role_cannot_be_deleted()
+    {
+        $defaultRole = Role::where('default', true)->first();
+
+        $this->actingAs($this->root)
+            ->delete(route('dashboard.develuserroles.roles.destroy', $defaultRole->key))
+            ->assertStatus(409);
+
+        $this->assertDatabaseHas('devel_user_roles', ['key' => $defaultRole->key]);
+    }
+
+    /** @test */
+    public function there_can_be_only_one_default_role()
+    {
+        $defaultRole = Role::where('default', true)->first();
+
+        // Create a new role
+        $data = [
+            'key' => 'test',
+            'name' => 'Test',
+            'permissions' => ['admin_dashboard.access'],
+            'default' => true,
+        ];
+
+        $this->actingAs($this->root)
+            ->postJson(route('dashboard.develuserroles.roles.store'), $data)
+            ->assertStatus(201);
+
+        // Make sure the new role is the default one and the original one is not
+        $newRole = Role::where('key', $data['key'])->first();
+
+        $this->assertEquals(true, $newRole->default);
+        $this->assertEquals(false, $defaultRole->fresh()->default);
+    }
+
+    /** @test */
+    public function there_has_to_be_a_default_role()
+    {
+        // Get the default role
+        $defaultRole = Role::where('default', true)->first();
+
+        // Try making it non-default
+        $data = [
+            'key' => $defaultRole->key,
+            'name' => $defaultRole->name,
+        ];
+
+        $this->actingAs($this->root)
+            ->post(route('dashboard.develuserroles.roles.update', $defaultRole->key), $data)
+            ->assertStatus(200);
+
+        // Make sure the role is still default
+        $this->assertEquals(1, $defaultRole->fresh->default);
+    }
 }
